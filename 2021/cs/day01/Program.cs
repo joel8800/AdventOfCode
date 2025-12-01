62d16c71133bc43138931700bf726c16d182d2fe
Joel Kaneshiro <joel.kaneshiro@teradyne.com>
2025-11-30 16:51:39 -0800

    Solved 2021 Day01

--- 2021/cs/day01/Program.cs
+++ 2021/cs/day01/Program.cs
@@ -1,15 +1,26 @@
 using Common;
 
-Console.WriteLine("Day 01: ");
+Console.WriteLine("Day 01: Sonar Sweep");
 
-string input = File.ReadAllText("inputSample.txt");
+List<int> depths = [.. InputReader.ReadInts("input.txt")];
 
+// get the difference between successive depths
+List<int> changes = [.. depths.Zip(depths.Skip(1), (a, b) => b - a)];
 
-int answerPt1 = 0;
+// count the number of positive changes
+int answerPt1 = changes.Where(change => change > 0).Count();
 
 // ----------------------------------------------------------------------------
 
-int answerPt2 = 0;
+// create a list of 3-measurement sliding window sums
+List<int> sums3;
+sums3 = [.. depths.Zip(depths.Skip(1), (a, b) => a + b)];
+sums3 = [.. sums3.Zip(depths.Skip(2), (a, b) => a + b)];
+
+// get the difference between successive window sums
+changes = [.. sums3.Zip(sums3.Skip(1), (a, b) => b - a)];
+
+int answerPt2 = changes.Where(change => change > 0).Count();
 
 Console.WriteLine($"Part 1: {answerPt1}");
 Console.WriteLine($"Part 2: {answerPt2}");
