bf60aaa8a07e3c00e09d517b3330d4490d182e90
Joel Kaneshiro <joel.kaneshiro@teradyne.com>
2025-12-01 19:01:30 -0800

    2025 Day01 python version converted from C#

--- 2025/py/day01/day01.py
+++ 2025/py/day01/day01.py
@@ -1,11 +1,33 @@
-﻿def main():
-    print("Day 01 - Advent of Code 2025")
-    try:
-        with open('input.txt') as f:
-            lines = f.read().splitlines()
-            print(f"Loaded {len(lines)} lines")
-    except FileNotFoundError:
-        print("No input.txt found.")
+﻿import sys
+
+def main():
+    print("Day 01: Secret Entrance")
+    
+    position = 50
+    zerosSeen = 0
+    endAtZero = 0
+    
+    with open('input.txt') as f:
+        
+        for line in f.read().splitlines():
+            
+            dir = line[0]
+            clicks = int(line[1:])
+            
+            for _ in range(clicks):
+                if dir == 'L':
+                    position = (position - 1 + 100) % 100
+                else:
+                    position = (position + 1) % 100
+                   
+                if position == 0:
+                    zerosSeen += 1
+            
+            if position == 0:
+                endAtZero += 1
+
+    print(f"Part1: {endAtZero}")
+    print(f"Part2: {zerosSeen}")
 
 if __name__ == '__main__':
     main()
