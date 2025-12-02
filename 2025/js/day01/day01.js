8ad8695a50ce932e21be0d1a2f4bb1eb251fea53
Joel Kaneshiro <joel.kaneshiro@teradyne.com>
2025-12-01 20:37:36 -0800

    2025 Day01 javascript version converted from C#

--- 2025/js/day01/day01.js
+++ 2025/js/day01/day01.js
@@ -1,10 +1,30 @@
 ﻿const fs = require('fs');
 
-console.log('Day 01 - Advent of Code 2025');
+console.log('Day 01: Secret Entrance');
 
-try {
-    const data = fs.readFileSync('input.txt', 'utf8').split('\\n');
-    console.log('Loaded ' + data.length + ' lines');
-} catch (err) {
-    console.log('No input.txt found.');
+const data = fs.readFileSync('input.txt', 'utf8').split('\r\n');
+
+let position = 50;
+let zerosSeen = 0;
+let endAtZero = 0;
+
+for (const line of data) 
+{
+    let dir = line[0] === 'L' ? -1 : 1;
+    let clicks = parseInt(line.slice(1), 10);
+
+    for (let i = 0; i < clicks; i++) {
+        position = (position + dir + 100) % 100;;
+        
+        if (position === 0) {
+            zerosSeen += 1;
+        }
+    }
+
+    if (position === 0) {
+        endAtZero += 1;
+    }
 }
+
+console.log('Part 1: ' + endAtZero);
+console.log('Part 2: ' + zerosSeen);
