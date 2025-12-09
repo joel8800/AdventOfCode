d73c4b6fbed71d9c93c30ccaa2d7d488580ccd91
Joel Kaneshiro <joel.kaneshiro@teradyne.com>
2025-12-08 16:55:18 -0800

    Solved 2015 Day07

---
+++ 2015/py/day07/day07.py
@@ -0,0 +1,13 @@
+﻿import sys
+def main():
+    print("Day 07: ")
+    
+    with open('inputSample.txt') as f:
+        lines = f.read().splitlines()
+        print(f"Loaded {len(lines)} lines")
+    
+    print(f"Part1: 7")
+    print(f"Part2: 7")
+
+if __name__ == '__main__':
+    main()
