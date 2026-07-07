97ab9598a370017e8d7fbf27c6d79dc95eabc1a7
Joel Kaneshiro <joel8800@gmail.com>
2026-07-07 16:55:38 -0700

    Optimized 2025 Day05

--- 2025/cs/day05/Program.cs
+++ 2025/cs/day05/Program.cs
@@ -3,15 +3,12 @@ using Common;
 Console.WriteLine("Day 05: Cafeteria");
 
 string[] input = InputReader.ReadBlocks("input.txt");
-string[] rangeLines = input[0].Split(Environment.NewLine);
 
 // parse ingredient ID ranges
-List<(long start, long end)> ranges = [];
-foreach (string line in rangeLines)
-{
+List<(long start, long end)> ranges = [.. input[0].Split(Environment.NewLine).Select(line => {
     string[] parts = line.Split('-');
-    ranges.Add((long.Parse(parts[0]), long.Parse(parts[1])));
-}
+    return (long.Parse(parts[0]), long.Parse(parts[1]));
+})];
 
 // parse ingredient IDs
 List<long> ingredients = [.. input[1].Split(Environment.NewLine).Select(line => long.Parse(line.Trim()))];
@@ -21,51 +18,33 @@ int fresh = 0;
 // check if each ingredient is in any of the ranges
 foreach (long ingredient in ingredients)
 {
-    foreach ((long start, long end) in ranges)
-    {
-        if (ingredient >= start && ingredient <= end)
-        {
-            fresh++;
-            break;
-        }
-    }
+    if (ranges.Any(r => ingredient >= r.start && ingredient <= r.end))
+        fresh++;
 }
 
 int answerPt1 = fresh;
 
 // ----------------------------------------------------------------------------
 
-
 // sort ranges by start value
-List<(long start, long end)> mergedRanges = [.. ranges.OrderBy(r => r.start)];
+List<(long start, long end)> sortedRanges = [.. ranges.OrderBy(r => r.start)];
+List<(long start, long end)> mergedRanges = [];
 
-// merge overlapping ranges
-for (int i  = 0; i < mergedRanges.Count - 1;  i++)
+// add ranges to mergedRanges list, if they overlap, extend the last range in mergedRanges
+foreach ((long start, long end) range in sortedRanges)
 {
-    for (int j = i + 1; j < mergedRanges.Count; j++)
+    if (mergedRanges.Count == 0 || range.start > mergedRanges[^1].end)
+        mergedRanges.Add(range);
+    else
     {
-        (long start, long end) r1 = mergedRanges[i];
-        (long start, long end) r2 = mergedRanges[j];
-        
-        if (r1.end >= r2.start)
-        {
-            long newStart = Math.Min(r1.start, r2.start);
-            long newEnd = Math.Max(r1.end, r2.end);
-            
-            mergedRanges[i] = (newStart, newEnd);
-            mergedRanges.RemoveAt(j);
-            j--;
-        }
+        // set the end of range to the largest value of the two overlapping ranges
+        var (start, end) = mergedRanges[^1];
+        mergedRanges[^1] = (start, Math.Max(range.end, end));
     }
 }
 
-long consideredFresh = 0;
-
 // get total number of IDs in all ranges
-foreach ((long start, long end) in mergedRanges)
-    consideredFresh += (end - start + 1);
-
-long answerPt2 = consideredFresh;
+long answerPt2 = mergedRanges.Sum(r => r.end - r.start + 1);
 
 Console.WriteLine($"Part 1: {answerPt1}");
 Console.WriteLine($"Part 2: {answerPt2}");
