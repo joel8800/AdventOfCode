0f530a95eb6dd6edc212b2c13d8960e66da54549
Joel Kaneshiro <joel8800@gmail.com>
2026-07-17 15:01:44 -0700

    Prepare 2015 day 09 workspace

---
+++ 2015/cs/day09/Program.cs
@@ -0,0 +1,17 @@
+using Common;
+
+Console.WriteLine("Day 09: All in a Single Night");
+
+string[] input = InputReader.ReadLines("inputSample.txt");
+
+
+int answerPt1 = 0;
+
+// ----------------------------------------------------------------------------
+
+int answerPt2 = 0;
+
+Console.WriteLine($"Part 1: {answerPt1}");
+Console.WriteLine($"Part 2: {answerPt2}");
+
+// ============================================================================
